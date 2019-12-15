6f5168908f3f4ff855938d1713db5bc63053659a
ninapart <57755772+ninapart@users.noreply.github.com>
2019-12-15 23:27:07 +0200

    Add files via upload

--- assignment2b/src/test/java/bgu/spl/mics/MessageBrokerTest.java
+++ assignment2b/src/test/java/bgu/spl/mics/MessageBrokerTest.java
@@ -4,46 +4,113 @@ import bgu.spl.mics.application.messages.GadgetAvailableEvent;
 import bgu.spl.mics.application.subscribers.M;
 import bgu.spl.mics.application.subscribers.Moneypenny;
 import bgu.spl.mics.application.subscribers.Q;
-import bgu.spl.mics.example.CallbackImpl;
+import bgu.spl.mics.example.messages.ExampleBroadcast;
 import bgu.spl.mics.example.messages.ExampleEvent;
-import jdk.nashorn.internal.codegen.CompilerConstants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import sun.plugin2.jvm.RemoteJVMLauncher;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.BooleanSupplier;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MessageBrokerTest {
-    Subscriber m;
-    Subscriber q;
-    Subscriber moneypenny;
-    MessageBroker messageBroker;
-    Event<GadgetAvailableEvent> gadgetsAvailableEventEvent;
+
+    MessageBrokerImpl messageBroker;
 
 
     @BeforeEach
-    public void setUp(){
-        m = new M();
-        q = new Q();
-        moneypenny = new Moneypenny();
+    public void setUp() {
         messageBroker = new MessageBrokerImpl();
-        gadgetsAvailableEventEvent = new GadgetAvailableEvent();
     }
 
     @Test
-    public void testSubscribeEvent(){
-//        Event<GadgetAvailableEvent> gadgetsAvailableEventEvent = new GadgetAvailableEvent();
-        messageBroker.subscribeEvent(Event<gadgetsAvailableEventEvent>, q);
-
+    public void getInstance() {
+        MessageBrokerImpl messBroker = new MessageBrokerImpl();
+        assertSame(messageBroker, messBroker.getInstance());
     }
 
-    <T> void subscribeEvent(Class<? extends Event<T>> type, Subscriber s);
-
-  
 
     @Test
-    public void unregisterTest(){
+    public void testSubscribeEvent() throws InterruptedException {
+        Subscriber q = new Q();
+        Event<String> event = new ExampleEvent("it's raining outside");
+        messageBroker.sendEvent(event);
+        assertTrue((messageBroker.awaitMessage(q) == null));
+        messageBroker.subscribeEvent(ExampleEvent.class, q);
+        messageBroker.sendEvent(event);
+        assertFalse(messageBroker.awaitMessage(q) == null);
+    }
+
+
+    @Test
+    public void subscribeBroadcast() throws InterruptedException {
+        Subscriber q = new Q();
+        ExampleBroadcast broadcast = new ExampleBroadcast("the diner is ready");
+        messageBroker.sendBroadcast(broadcast);
+        assertTrue((messageBroker.awaitMessage(q) == null));
+        messageBroker.subscribeBroadcast(ExampleBroadcast.class, q);
+        messageBroker.sendBroadcast(broadcast);
+        assertFalse(messageBroker.awaitMessage(q) == null);
+    }
+
+
+    @Test
+    public void complete() {
+        Future future = new Future();
+        future.resolve(true);
+        Event<String> event = new ExampleEvent("My name is Bond, Jame Bond");
+        assertTrue((future.get() == null));
+        messageBroker.complete(event, "true");
+        assertFalse(future.get() == null);
+    }
+
+
+    @Test
+    public void sendEvent() throws InterruptedException {
+        Subscriber q = new Q();
+        Subscriber m = new M();
+        Event<String> event = new ExampleEvent("who are you ?");
+        messageBroker.subscribeEvent(ExampleEvent.class, q);
+        messageBroker.subscribeEvent(ExampleEvent.class, m);
+        assertTrue((messageBroker.awaitMessage(q) == null) & (messageBroker.awaitMessage(m) == null));
+        messageBroker.sendEvent(event);
+        assertFalse(((messageBroker.awaitMessage(q) == null) & !(messageBroker.awaitMessage(m) == null)) ||
+                (!(messageBroker.awaitMessage(q) == null) & (messageBroker.awaitMessage(m) == null)));
+        ;
+    }
+
+
+    @Test
+    public void sendBroadcast() throws InterruptedException {
+        Subscriber q = new Q();
+        Broadcast broadcast = new ExampleBroadcast("Hello beautiful world");
+        messageBroker.subscribeBroadcast(ExampleBroadcast.class, q);
+        assertTrue(messageBroker.awaitMessage(q) == null);
+        messageBroker.sendBroadcast(broadcast);
+        assertFalse(messageBroker.awaitMessage(q) == null);
+    }
+
+
+    @Test
+    public void register() throws InterruptedException {
+        Subscriber q = new Q();
+        messageBroker.subscribeEvent(ExampleEvent.class, q);
+        Event<String> event = new ExampleEvent("who are you ?");
+        messageBroker.sendEvent(event);
+        assertTrue(messageBroker.awaitMessage(q) == null);
+        messageBroker.register(q);
+        messageBroker.subscribeEvent(ExampleEvent.class, q);
+        Event<String> event2 = new ExampleEvent("who are you guy?");
+        messageBroker.sendEvent(event);
+        assertFalse(messageBroker.awaitMessage(q) == null);
+    }
+
+
+    @Test
+    public void unregisterTest() throws InterruptedException {
         Subscriber q = new Q();
         messageBroker.subscribeEvent(ExampleEvent.class, q);
         messageBroker.register(q);
@@ -59,7 +126,7 @@ public class MessageBrokerTest {
     }
 
     @Test
-    public void awaitMessageTest(){
+    public void awaitMessageTest() throws InterruptedException {
         Subscriber q = new Q();
         try{
             messageBroker.awaitMessage(q);
@@ -73,5 +140,6 @@ public class MessageBrokerTest {
         assertEquals(event, messageBroker.awaitMessage(q));
     }
 
-
 }
+
+
