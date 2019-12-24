902e8ec79ab5960b96f0819337242f8c032183d8
ninapart <57755772+ninapart@users.noreply.github.com>
2019-12-24 12:23:56 +0200

    Add files via upload

--- assignment2b/src/main/java/bgu/spl/mics/application/publishers/Intelligence.java
+++ assignment2b/src/main/java/bgu/spl/mics/application/publishers/Intelligence.java
@@ -1,15 +1,15 @@
-package bgu.spl.mics.application.publishers;
+package bgu.spl.mics.application.subscribers;
 
-import bgu.spl.mics.Callback;
-import bgu.spl.mics.Publisher;
-import bgu.spl.mics.SimplePublisher;
-import bgu.spl.mics.Subscriber;
+import bgu.spl.mics.*;
 import bgu.spl.mics.application.messages.AgentsAvailableEvent;
 import bgu.spl.mics.application.messages.MissionReceivedEvent;
 import bgu.spl.mics.application.messages.TickBroadcast;
+import bgu.spl.mics.application.passiveObjects.Gadget;
 import bgu.spl.mics.application.passiveObjects.MissionInfo;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -29,70 +29,71 @@ public class Intelligence extends Subscriber {
 
 	//***********************fields
 	private List<MissionInfo> missionInfoList;
-	private int nextSendingTimeTick;
-	private int currentTimeTick;
-	private int nextMissionToSend; //by index in missioninfolist
+	//private int nextSendingTimeTick;
+	private int currentGlobalTime;
+	private int currentIndex; //by index in missioninfolist
 	private boolean anymoreMissions;
+	private MissionInfo currentMission;
 
 	//init
 	public Intelligence() {
 		super("Intelligence");
-		//fill missioninfolist
-
-
+		missionInfoList = new ArrayList<>();
+		anymoreMissions = true;
+		currentIndex = 0;
+		currentGlobalTime = 0;
+		currentMission = missionInfoList.get(currentIndex);
 
 
 		// TODO Implement this according to json?
 	}
 
-
-	@Override
 	protected void initialize() {
 		sortMissionInfoList();
-		if (!missionInfoList.isEmpty()){
-			anymoreMissions = true;
-			nextMissionToSend = 0;
-			nextSendingTimeTick = missionInfoList.get(0).getTimeIssued();
-		}
-		//TODO subscribe broadcast
+		Callback<TickBroadcast> tickBroadcastCallback = new TickCallback();
+		subscribeBroadcast(TickBroadcast.class, tickBroadcastCallback); //subscribing to tick broadcast
+	}
 
 
-		Callback<MissionReceivedEvent> callback = new Callback<MissionReceivedEvent>(){
-			@Override //TODO do we need to throw exception?
-			public void call(MissionReceivedEvent e) throws InterruptedException {
+	/**
+	 * the callback function of selling service that should be called when receiving a tick broadcast.
+	 * send a BookOrderEvent if the schedule contains an order with the same tick received from the broadcast.
+	 */
+	private class TickCallback implements Callback<TickBroadcast> {
+		@Override
+		public void call(TickBroadcast tb) {
+			currentGlobalTime = tb.getTime();
 
-				complete(e,);
+			//loop in 'while' because some orders have the same tick
+			while (anymoreMissions) {
+				while (currentMission.getTimeIssued() == currentGlobalTime) {  //its the time to send the order for this tick
+					String missionName = currentMission.getMissionName();
+					List<String> agents = currentMission.getSerialAgentsNumbers();
+					String gadget = currentMission.getGadget();
+					MissionReceivedEvent missionToSend = new MissionReceivedEvent(missionName, gadget, agents);
+					simplePublisher.sendEvent(missionToSend); //TODO make sure its okay that we ignore the returned Future
+					currentIndex++;
+					if (currentIndex + 1 == missionInfoList.size()) {
+						anymoreMissions = false;
+						break;
+					}
+					currentMission = missionInfoList.get(currentIndex);
+
+				}
 			}
-		};
-		this.subscribeEvent(AgentsAvailableEvent.class, callback);
-
-//		TickBroadcast type = new TickBroadcast();
-
-		this.subscribeBroadcast(TickBroadcast.class, callback);
-/*
-//TODO : not sure its necessary
-		//take agents from the squad
-		Callback<TakeAgentsEvent> callback2 = (takeAgentsEvent)-> {
-			List<String> agents = squad.takeAgents(takeAgentsEvent.getName());
-			complete(takeAgentsEvent, agents);
-		};
-		subscribeEvent(TakeGadgetEvent.class, callback2);
- */
-		initialized = true;
+		}
 	}
+
+
+		//sort missionInfoList according to timeIssued
+		private void sortMissionInfoList() {
+			missionInfoList.sort(Comparator.comparing(MissionInfo::getTimeIssued));
+		}
+
+		public void addMission(MissionInfo mi) {
+			missionInfoList.add(mi);
+		}
 
 	}
 
 
-
-
-	//sort missionInfoList according to timeIssued
-	private void sortMissionInfoList(){
-		missionInfoList.sort(Comparator.comparing(MissionInfo::getTimeIssued));
-	}
-
-	public void addMission(MissionInfo mi) {
-		missionInfoList.add(mi);
-	}
-
-}
