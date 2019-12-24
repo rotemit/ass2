239ca0f038261e2cbb654ff82787425b0d536bac
ninapart <57755772+ninapart@users.noreply.github.com>
2019-12-24 23:00:22 +0200

    Add files via upload

--- assignment2b/src/main/java/bgu/spl/mics/application/MI6Runner.java
+++ assignment2b/src/main/java/bgu/spl/mics/application/MI6Runner.java
@@ -1,3 +1,4 @@
+/*
 package bgu.spl.mics.application;
 
 import bgu.spl.mics.MessageBrokerImpl;
@@ -12,12 +13,13 @@ import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
-/** This is the Main class of the application. You should parse the input file,
- * create the different instances of the objects, and run the system.
- * In the end, you should output serialized objects.
- */
+// This is the Main class of the application. You should parse the input file,
+// create the different instances of the objects, and run the system.
+//  In the end, you should output serialized objects.
+
 public class MI6Runner {
     public static void main(String[] args) {
 
@@ -36,15 +38,15 @@ public class MI6Runner {
 
             //initialize Ms and Moneypennys
             //TODO check which strategy works for reading the data inside the "services" JsonObject
-            /*
-            JsonArray jsonServices = input.getAsJsonArray("services");
-            Integer mInstances = Integer.parseInt(String.valueOf(jsonServices.get(0)));
-            Integer mpInstances = Integer.parseInt(String.valueOf(jsonServices.get(1)));
-            */
+
+//            JsonArray jsonServices = input.getAsJsonArray("services");
+//            Integer mInstances = Integer.parseInt(String.valueOf(jsonServices.get(0)));
+//            Integer mpInstances = Integer.parseInt(String.valueOf(jsonServices.get(1)));
+
 
             JsonObject jsonServices = input.getAsJsonObject("services");
             Integer mInstances = jsonServices.get("M").getAsInt();
-            Integer mpInstances = jsonServices.get("MoneyPenny").getAsInt();
+            Integer mpInstances = jsonServices.get("Moneypenny").getAsInt();
 
 
             //initialize Ms
@@ -66,7 +68,6 @@ public class MI6Runner {
             //TODO next line might be buggy, maybe the needed command is getasjsonarray
             //different option for creating intelligenceInServices array
 //            JsonArray intelligenceInServices = (JsonArray) jsonServices.get(2);
-
 
 
             for (int i = 0; i < intelligenceInServices.size(); i++) {
@@ -92,22 +93,95 @@ public class MI6Runner {
 
                 }
             }
-            //initialize Intelligence
-//            JsonArray intelligencesArray = jsonServices.getAsJsonArray("intelligence");
-            for (int i = 0; i < jsonServices.size(); i++) {
-                JsonObject currIntelligence = jsonServices.get(i).getAsJsonObject();
-                Intelligence
-                for (int j = 0; j)
-            }
 
-            JsonObject jsonIntelligences = jsonServices.get(0).getAsJsonObject();
-            JsonArray intelligencesArray
-        } catch (Exception e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 }
+*/
 
 
+
+
+
+
+
+
+
+
+
+package bgu.spl.mics.application;
+
+import bgu.spl.mics.application.passiveObjects.Diary;
+import bgu.spl.mics.application.passiveObjects.Inventory;
+import bgu.spl.mics.example.Creator;
+import bgu.spl.mics.example.publishers.ExampleMessageSender;
+import bgu.spl.mics.example.subscribers.ExampleBroadcastSubscriber;
+import bgu.spl.mics.example.subscribers.ExampleEventHandlerSubscriber;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.sun.corba.se.pept.encoding.InputObject;
+import jdk.nashorn.internal.parser.JSONParser;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+// This is the Main class of the application. You should parse the input file,
+//  create the different instances of the objects, and run the system.
+//  In the end, you should output serialized objects.
+
+public class MI6Runner {
+
+    public static void main(String[] args) {
+        System.out.println("4");
+        MI6Runner runner = new MI6Runner();
+        String inputInJson = null;
+        try {
+            inputInJson = runner.readFile(args[0]);  //getting the input string, which is currently in Json format
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Gson gson = new Gson();
+        Type founderListType = new TypeToken<InputObject>(){}.getType();
+
+
+        InputObject info = gson.fromJson(inputInJson,founderListType);  //converting the string containing the input as Json to an object, which we can retrieve the info from easily
+        System.out.println("finishing");
+    }
+
+
+//      @param fileName the name of the file which we should read from
+//      @return a String which is the text of the file filename
+//      @throws IOException
+
+
+    public String readFile(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append("\n");
+                line = br.readLine();
+            }
+            return sb.toString();
+        } finally {
+            br.close();
+        }
+    }
+
+
+}
 
 
