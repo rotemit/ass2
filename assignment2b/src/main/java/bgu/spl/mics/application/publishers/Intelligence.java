a2dc15eb9895699214ac54d40455787ceb83b0dc
rotemit <57985126+rotemit@users.noreply.github.com>
2019-12-24 10:50:55 +0200

    Add files via upload

--- assignment2b/src/main/java/bgu/spl/mics/application/publishers/Intelligence.java
+++ assignment2b/src/main/java/bgu/spl/mics/application/publishers/Intelligence.java
@@ -1,7 +1,16 @@
 package bgu.spl.mics.application.publishers;
 
+import bgu.spl.mics.Callback;
 import bgu.spl.mics.Publisher;
+import bgu.spl.mics.SimplePublisher;
 import bgu.spl.mics.Subscriber;
+import bgu.spl.mics.application.messages.AgentsAvailableEvent;
+import bgu.spl.mics.application.messages.MissionReceivedEvent;
+import bgu.spl.mics.application.messages.TickBroadcast;
+import bgu.spl.mics.application.passiveObjects.MissionInfo;
+
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * A Publisher only.
@@ -10,21 +19,80 @@ import bgu.spl.mics.Subscriber;
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
  */
-public class Intelligence extends Publisher {
+public class Intelligence extends Subscriber {
+/* herited
+    private final String name;
+    private final SimplePublisher simplePublisher;
+    protected MessageBroker messageBroker;
+     */
 
+
+	//***********************fields
+	private List<MissionInfo> missionInfoList;
+	private int nextSendingTimeTick;
+	private int currentTimeTick;
+	private int nextMissionToSend; //by index in missioninfolist
+	private boolean anymoreMissions;
+
+	//init
 	public Intelligence() {
-		super("Change_This_Name");
-		// TODO Implement this
+		super("Intelligence");
+		//fill missioninfolist
+
+
+
+
+		// TODO Implement this according to json?
 	}
+
 
 	@Override
 	protected void initialize() {
-		// TODO Implement this
+		sortMissionInfoList();
+		if (!missionInfoList.isEmpty()){
+			anymoreMissions = true;
+			nextMissionToSend = 0;
+			nextSendingTimeTick = missionInfoList.get(0).getTimeIssued();
+		}
+		//TODO subscribe broadcast
+
+
+		Callback<MissionReceivedEvent> callback = new Callback<MissionReceivedEvent>(){
+			@Override //TODO do we need to throw exception?
+			public void call(MissionReceivedEvent e) throws InterruptedException {
+
+				complete(e,);
+			}
+		};
+		this.subscribeEvent(AgentsAvailableEvent.class, callback);
+
+//		TickBroadcast type = new TickBroadcast();
+
+		this.subscribeBroadcast(TickBroadcast.class, callback);
+/*
+//TODO : not sure its necessary
+		//take agents from the squad
+		Callback<TakeAgentsEvent> callback2 = (takeAgentsEvent)-> {
+			List<String> agents = squad.takeAgents(takeAgentsEvent.getName());
+			complete(takeAgentsEvent, agents);
+		};
+		subscribeEvent(TakeGadgetEvent.class, callback2);
+ */
+		initialized = true;
 	}
 
-	@Override
-	public void run() {
-		// TODO Implement this
+	}
+
+
+
+
+	//sort missionInfoList according to timeIssued
+	private void sortMissionInfoList(){
+		missionInfoList.sort(Comparator.comparing(MissionInfo::getTimeIssued));
+	}
+
+	public void addMission(MissionInfo mi) {
+		missionInfoList.add(mi);
 	}
 
 }
--- assignment2b/src/main/java/bgu/spl/mics/application/publishers/TimeService.java
+++ assignment2b/src/main/java/bgu/spl/mics/application/publishers/TimeService.java
@@ -1,11 +1,15 @@
 package bgu.spl.mics.application.publishers;
 
 import bgu.spl.mics.Publisher;
+import bgu.spl.mics.application.messages.TickBroadcast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * TimeService is the global system timer There is only one instance of this Publisher.
  * It keeps track of the amount of ticks passed since initialization and notifies
- * all other subscribers about the current time tick using {@link Tick Broadcast}.
+ * all other subscribers about the current time tick using {@link TickBroadcast}.
  * This class may not hold references for objects which it is not responsible for.
  * 
  * You can add private fields and public methods to this class.
@@ -13,20 +17,46 @@ import bgu.spl.mics.Publisher;
  */
 public class TimeService extends Publisher {
 
+	//***************fields
+	Timer timer;
+	int totalTicks;
+	int currentTimeTick;
+
 	public TimeService() {
-		super("Change_This_Name");
+		super("TimeService");
 		// TODO Implement this
+	}
+
+	public TimeService(int ticks){
+		super("TimeService");
+		totalTicks = ticks;
+		timer = new Timer();
 	}
 
 	@Override
 	protected void initialize() {
-		// TODO Implement this
-		
+		TimerTask task = new TimerTask(){
+			@Override
+			public void run(){
+				if (currentTimeTick<totalTicks){
+					TickBroadcast tb = new TickBroadcast(currentTimeTick);
+					getSimplePublisher().sendBroadcast(tb);
+					currentTimeTick++;
+				}
+				else {
+					//terminate
+					timer.cancel();
+//					task.cancel();
+					//TODO maybe more terminations are required
+				}
+			}
+		};
+		timer.scheduleAtFixedRate(task, 0, 100); //delay 0, 100 milliseconds between successive executions
 	}
 
 	@Override
 	public void run() {
-		// TODO Implement this
+		initialize();
 	}
 
 }
