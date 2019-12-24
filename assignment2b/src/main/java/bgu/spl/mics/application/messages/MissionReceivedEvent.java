92fa91f099ddc6c882897e9852ac1891e6faba16
rotemit <57985126+rotemit@users.noreply.github.com>
2019-12-24 10:49:25 +0200

    Add files via upload

--- assignment2b/src/main/java/bgu/spl/mics/application/messages/MissionReceivedEvent.java
+++ assignment2b/src/main/java/bgu/spl/mics/application/messages/MissionReceivedEvent.java
@@ -3,15 +3,31 @@ package bgu.spl.mics.application.messages;
 import bgu.spl.mics.Event;
 import bgu.spl.mics.Message;
 
+import java.util.List;
+
 public class MissionReceivedEvent implements Event {
     private String missionName;
+    private String gadget;
+    private List<String> agentsList; //TODO check if this needs to be a list or a single agent (according to forum we can decide)
 
-    public MissionReceivedEvent(String missionName) {
+    public MissionReceivedEvent(String missionName, String gadget, List<String> agentsList) {
         this.missionName = missionName;
+        this.gadget = gadget;
+        this.agentsList = agentsList;
     }
 
     public String getMissionName() {
         return missionName;
     }
+
+    public String getGadget(){
+        return gadget;
+    }
+
+    public List<String> getAgentsList(){
+        return agentsList;
+    }
+
+
 
 }
